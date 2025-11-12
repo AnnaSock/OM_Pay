979d6321dd3b0000999aeb807207301ea77a7e2a
AnnaSock <annasock542@gmail.com>
2025-11-12 13:04:38 +0000

    fatigué

--- config/cors.php
+++ config/cors.php
@@ -23,7 +23,7 @@ return [
 
     'allowed_origins_patterns' => [],
 
-    'allowed_headers' => ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept', 'Origin', 'X-CSRF-TOKEN'],
+    'allowed_headers' => ["*"],
 
     'exposed_headers' => ['X-RateLimit-Limit', 'X-RateLimit-Remaining', 'X-RateLimit-Reset'],
 
